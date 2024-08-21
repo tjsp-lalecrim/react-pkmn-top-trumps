2635034e6f1d18449788cc13a2ef308247e981bb
Leandro Moraes de Alecrim <lalecrim@tjsp.jus.br>
2024-08-21 13:44:38 -0300

    fix Log key

--- src/components/Log.js
+++ src/components/Log.js
@@ -2,7 +2,7 @@ export default function Log({ log }) {
     return (
         <div className='w-full bg-slate-50 text-center text-slate-950 rounded py-1 my-2'>
             {
-                log.map((value, index) => <p id={`log-${index}`}>{value}</p>)
+                log.map((item, index) => <p key={`log-${index}`}>{item}</p>)
             }
         </div>
     );
