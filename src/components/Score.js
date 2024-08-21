7164c08e2f58026652c460def637063c25a7c667
Leandro Moraes de Alecrim <lalecrim@tjsp.jus.br>
2024-08-21 13:11:32 -0300

    Score Layout

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,18 +1,14 @@
-export default function Score({yourScore, opponentScore}){
+export default function Score({ yourScore, opponentScore }) {
     return (
-        <table className='w-full flex flex-col items-center'>
-                <thead className='flex justify-items-between'>
-                    <tr>
-                        <th className="text-center">You</th>
-                        <th className="text-center">Opponent</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td className="text-center">{yourScore || 0}</td>
-                        <td className="text-center">{opponentScore || 0}</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div className='w-full flex justify-center'>
+            <div className='w-full flex flex-col items-center justify-evenly'>
+                <span >You</span>
+                <span>{yourScore || 0}</span>
+            </div>
+            <div className='w-full flex flex-col items-center justify-evenly'>
+                <span >Opponent</span>
+                <span>{opponentScore || 0}</span>
+            </div>
+        </div>
     );
 }
